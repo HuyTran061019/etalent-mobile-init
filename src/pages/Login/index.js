a3b354aa60b0575a51789463ad3ad8fe9fad5f0c
HuyTran1412 <57608002+HuyTran061019@users.noreply.github.com>
2024-01-04 15:47:17 +0700

    oauth

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,27 +4,43 @@ import { navigationNavigate } from "../../services/NavigationServices";
 import { HomeName } from "../../constants/ScreenNames";
 import masterStyles from "../../styles/masterStyles";
 import { SafeAreaView } from "react-native-safe-area-context";
+import LinkedInModal from "@gcou/react-native-linkedin";
 
 const LoginPage = () => {
   return (
     <SafeAreaView style={masterStyles.container}>
       <Text>Hello</Text>
-      <Pressable style={styles.buttonContainer} onPress={() => navigationNavigate(HomeName)}>
+      <Pressable
+        style={styles.buttonContainer}
+        onPress={() => navigationNavigate(HomeName)}
+      >
         <Text>To Home </Text>
       </Pressable>
+
+      <LinkedInModal
+        // ref={this.linkedRef}
+        clientID="7816sqyqx9hwe3"
+        shouldGetAccessToken={false}
+        clientSecret={null}
+        redirectUri="http://localhost:8081/auth/linkedin/callback"
+        permissions={["r_liteprofile", "r_emailaddress"]}
+        // onSuccess={(res) => console.log(res)}
+        onSuccess={({ authentication_code }) => console.log(authentication_code)}
+        onError={(error) => console.log(error)}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        paddingTop: 12,
-        paddingBottom: 12,
-        paddingRight: 16,
-        paddingLeft: 16,
-        backgroundColor: 'grey',
-        marginTop: 12,
-    }
-})
+  buttonContainer: {
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingRight: 16,
+    paddingLeft: 16,
+    backgroundColor: "grey",
+    marginTop: 12,
+  },
+});
 
 export default LoginPage;
